72cbbcd94cbd36245b091e8ce0fd8ffc7d9f2a2c
Manoel C <manoelclemente9@outlook.com>
2023-02-12 19:58:30 -0300

    utilizando autowired

--- src/main/java/com/example/demoapi/service/AtivacaoClienteService.java
+++ src/main/java/com/example/demoapi/service/AtivacaoClienteService.java
@@ -2,20 +2,19 @@ package com.example.demoapi.service;
 
 import com.example.demoapi.model.Cliente;
 import com.example.demoapi.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@Component
 public class AtivacaoClienteService {
 
-
+    @Autowired
     private Notificador notificador;
-
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-    }
 
     public void ativar(Cliente cliente){
         cliente.ativar();
 
         notificador.notificar(cliente,"seu cadastro no sistema está ativo!");
     }
+
 }
